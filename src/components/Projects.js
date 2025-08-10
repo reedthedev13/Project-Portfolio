20087742023d58bd0889b3e3f72777932d34dcad
Reed Lathrop <reedlathrop@gmail.com>
2025-08-10 14:26:35 -0500

    image fixes + project re-arrange

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,26 +1,26 @@
 import React from "react";
 import ProjectCard from "../components/ProjectCard";
-import NoteTaking2 from "../assets/NoteTakeSS2.jpg";
-import ToDoList2 from "../assets/ToDoSS2.jpg";
-import StockWatch2 from "../assets/StockWatchSS2.jpg";
-import JobAppPic2 from "../assets/JobAppSS2.jpg";
-import MealPlanPic from "../assets/MealPlanSS2.jpg";
-import FitnessTracker from "../assets/FitnessTrackSS2.jpg";
-import WeatherApp2 from "../assets/WeatherAppSS2.jpg";
-import GolfScore2 from "../assets/GolfScoreSS2.jpg";
+import NoteTaking3 from "../assets/NoteTakeSS3.jpg";
+import ToDoList3 from "../assets/ToDoSS3.jpg";
+import StockWatch3 from "../assets/StockWatchSS3.jpg";
+import JobAppPic3 from "../assets/JobAppSS3.jpg";
+import MealPlanPic from "../assets/MealPlanSS3.jpg";
+import FitnessTracker from "../assets/FitnessTrackSS3.jpg";
+import WeatherApp3 from "../assets/WeatherAppSS3.jpg";
+import GolfScore3 from "../assets/GolfScoreSS3.jpg";
 import FinanceTracker3 from "../assets/FinanceTrackerSS3.jpg";
 
 const Projects = () => {
   const projects = [
     {
       id: 1,
-      title: "Golf Score Card",
+      title: "Fitness Tracker",
       description:
-        "An online tool that gives you the ability to score and store 18-hole golf rounds.",
-      technologies: ["ReactJS", "TailwindCSS"],
-      image: GolfScore2,
-      githubLink: "https://github.com/reedthedev13/Golf-Scorecard-",
-      liveDemo: "https://golf-score-card-zeta.vercel.app/",
+        "Track workouts by type, date, and reps. Includes analytics and charts - (App may load slowly due to backend hosting service)",
+      technologies: ["React", "Python", "TailwindCSS", "REST API"],
+      image: FitnessTracker,
+      githubLink: "https://github.com/reedthedev13/fitess-tracker",
+      liveDemo: "https://fitess-tracker.vercel.app/",
     },
     {
       id: 2,
@@ -34,45 +34,16 @@ const Projects = () => {
     },
     {
       id: 3,
-      title: "Weather Forecast App",
-      description:
-        "Fully functional 14-day weather forecast for St. Louis, Missouri.",
-      technologies: ["React", "JavaScript", "TailwindCSS", "API integration"],
-      image: WeatherApp2,
-      githubLink: "https://github.com/reedthedev13/14-day-weather",
-      liveDemo: "https://14-day-weather.vercel.app/",
-    },
-    {
-      id: 4,
-      title: "Note-Taking Web App",
-      description: "Allows users to create and store notes.",
-      technologies: ["React", "JavaScript", "TailwindCSS"],
-      image: NoteTaking2,
-      githubLink: "https://github.com/reedthedev13/note-take-app-fix",
-      liveDemo: "https://note-take-app-fix.vercel.app/",
-    },
-    {
-      id: 5,
-      title: "Fitness Tracker",
-      description:
-        "Track workouts by type, date, and reps. Includes analytics and charts - (App may load slowly due to backend hosting service)",
-      technologies: ["React", "Python", "TailwindCSS", "REST API"],
-      image: FitnessTracker,
-      githubLink: "https://github.com/reedthedev13/fitess-tracker",
-      liveDemo: "https://fitess-tracker.vercel.app/",
-    },
-    {
-      id: 6,
       title: "To-Do List",
       description:
         "Organize tasks into categories and persist data - (App may load slowly due to backend hosting service)",
       technologies: ["ReactJS", "TailWindCSS", "GO", "Gin"],
-      image: ToDoList2,
+      image: ToDoList3,
       githubLink: "https://github.com/reedthedev13/To-do-list-",
       liveDemo: "https://to-do-list-tau-taupe-69.vercel.app/",
     },
     {
-      id: 7,
+      id: 4,
       title: "Recipe & Meal Planner",
       description:
         "Users can browse, add, and save recipes. Generate weekly meal plans. & view nutritional info",
@@ -82,12 +53,41 @@ const Projects = () => {
       liveDemo: "https://meal-planner-app-sepia.vercel.app/",
     },
     {
+      id: 5,
+      title: "Note-Taking Web App",
+      description: "Allows users to create and store notes.",
+      technologies: ["React", "JavaScript", "TailwindCSS"],
+      image: NoteTaking3,
+      githubLink: "https://github.com/reedthedev13/note-take-app-fix",
+      liveDemo: "https://note-take-app-fix.vercel.app/",
+    },
+    {
+      id: 6,
+      title: "Weather Forecast App",
+      description:
+        "Fully functional 14-day weather forecast for St. Louis, Missouri.",
+      technologies: ["React", "JavaScript", "TailwindCSS", "API integration"],
+      image: WeatherApp3,
+      githubLink: "https://github.com/reedthedev13/14-day-weather",
+      liveDemo: "https://14-day-weather.vercel.app/",
+    },
+    {
+      id: 7,
+      title: "Golf Score Card",
+      description:
+        "An online tool that gives you the ability to score and store 18-hole golf rounds.",
+      technologies: ["ReactJS", "TailwindCSS"],
+      image: GolfScore3,
+      githubLink: "https://github.com/reedthedev13/Golf-Scorecard-",
+      liveDemo: "https://golf-score-card-zeta.vercel.app/",
+    },
+    {
       id: 8,
       title: "Job Application Tracker",
       description:
         "Add & update job applications with status (applied, interviewing, offer, rejected).",
       technologies: ["TypeScript", "React", "TailWindCSS", "Firebase"],
-      image: JobAppPic2,
+      image: JobAppPic3,
       githubLink: "https://github.com/reedthedev13/Job-Application-Tracker2",
       liveDemo: "https://job-application-tracker2.vercel.app/",
     },
@@ -97,7 +97,7 @@ const Projects = () => {
       description:
         "Search and create a watchlist of stocks. View and 24hr change.",
       technologies: ["TypeScript", "React", "TailWindCSS", "TwelveData API"],
-      image: StockWatch2,
+      image: StockWatch3,
       githubLink: "https://github.com/reedthedev13/Stock-Watchlist-",
       liveDemo: "https://stock-watchlist-alpha.vercel.app/",
     },
